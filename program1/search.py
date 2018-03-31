6bb0c36659bd9c23f4d3e42bfd74d3b083faacd1
cysun0226 <j6256448@gmail.com>
2018-04-01 00:12:56 +0800

    Add class 'Sequence'

--- program1/search.py
+++ program1/search.py
@@ -12,8 +12,52 @@ sequence = []
 # struct
 Point = namedtuple("Point", "x y")
 
-# BFS
+class Sequence(object):
+    def __init__(self, id, dist):
+        self.direction = 'SKIP'
+        self.id = id
+        self.distance = dist
+    def __str__(self):
+        output = '(' + self.direction
+        if self.direction != 'SKIP':
+            if self.distance >= 0:
+                output += '+'
+            else:
+                output += '-'
+        output += ') ' + str(self.distance)
 
+        return output
+
+
+
+# BFS
+# def BFS(seq, target):
+#     start = Point(0,0)
+#
+#     # keep track of all visited nodes
+#     explored = []
+#     # keep track of nodes to be checked
+#     queue = [start]
+#
+#     levels = {}         # this dict keeps track of levels
+#     levels[start] = 0    # depth of start node is 0
+#
+#     visited = [start]     # to avoid inserting the same node twice into the queue
+#
+#     # keep looping until there are nodes still to be checked
+#     while queue:
+#        # pop shallowest node (first node) from queue
+#         node = queue.pop(0)
+#         explored.append(node)
+#         neighbours = graph[node]
+#
+#         # add neighbours of node to queue
+#         for neighbour in neighbours:
+#             if neighbour not in visited:
+#                 queue.append(neighbour)
+#                 visited.append(neighbour)
+#
+#     return
 
 # main
 seq_file = open(seq_filename, "r")
@@ -22,20 +66,12 @@ seq_file.close()
 
 for line in lines:
     line = line.split()
-    print(line)
     strategy = line[0]
     del line[0]
-    print("strategy = " + strategy)
     target = Point(int(line[0]), int(line[1]))
-    print("target = ", end="")
-    print(target)
-    print(target.x)
-    print(target.y)
+    #print(target.x)
+    #print(target.y)
     del line[0:2]
-    # print(line)
     seq = []
-    print("seq = ")
-    for num in line:
-        seq.append(int(num))
-    print(seq)
-    print()
+    for i in range(len(line)):
+        seq.append(Sequence(i,int(line[i])))
