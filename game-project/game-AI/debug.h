6d81aa2939161b6d619b0e0321243418909c2ff3
cysun0226 <j6256448@gmail.com>
2018-05-02 03:11:44 +0800

    devide debug to .h and .cpp (for using in multiple files)

--- game-project/game-AI/debug.h
+++ game-project/game-AI/debug.h
@@ -1,32 +1,21 @@
 // functions for debug
 // by cysun
 
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-void waitKey() {
-  cout << "\nPress any key to continue ...";
-  cin.get();
-  cout << endl;
-}
+void waitKey();
 
 template <class vType>
-void printVector(std::vector<vType> v, string name) {
-  cout << "\n -- " << name << " -- \n" << endl;
-  for (size_t i = 0; i < v.size(); i++) {
-    cout << name << "[" << i << "] = " << v[i] << endl;
-  }
-  cout << endl;
-}
+void printVector(std::vector<vType> v, string name);
 
 template <class vType>
-void printPointerVector(std::vector<vType> v, string name) {
-  cout << "\n -- " << name << " -- \n" << endl;
-  for (size_t i = 0; i < v.size(); i++) {
-    cout << name << "[" << i << "] = " << *v[i] << endl;
-  }
-  cout << endl;
-}
+void printPointerVector(std::vector<vType> v, string name);
+
+#endif
